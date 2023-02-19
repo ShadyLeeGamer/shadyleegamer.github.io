b89260bfbcf0a0838b077e7fce39cd2b6faf4220
ShadyLeeGamer <64636379+ShadyLeeGamer@users.noreply.github.com>
2023-02-19 01:08:49 +0000

    remove old pages

--- a/NEW/green/assets/js/main.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e39a4e3a45580c0090bd07f8798e2a8421fc1dcb978f3628b28835b63e3e51de
-size 1704
--- a/assets/js/main.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0eb987e6300fde9545edd2c2181d0fae47ec56b16bdf0a8da3078e1f134813b2
-size 3872
