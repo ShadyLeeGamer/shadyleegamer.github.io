197b934bea939e745cf392436097f1cb35b37965
ShadyLeeGamer <64636379+ShadyLeeGamer@users.noreply.github.com>
2023-07-18 21:25:10 +0100

    loader test 2

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,9 +1,13 @@
 const projectTop = document.getElementById('project-top');
 isHomePage = projectTop == null;
+$("#loader-wrapper").show();
+// $(document).ready(function() {
+//     $("#loader-wrapper").fadeOut("slow");
+//     setTimeout(BigReelIntro, isHomePage ? 2000 : 1000);
+// });
 $(window).on("load", function() {
     $("#loader-wrapper").fadeOut("slow");
-    setTimeout(BigReelIntro, isHomePage ? 2000 : 1000);
-});
+  });
 
 // Big Reel
 function BigReelIntro() {
@@ -15,12 +19,17 @@ function BigReelIntro() {
     // Home page
     if (isHomePage)
     {
+				// height: 28em;
         bigReelContainer.style.height = bigReelVideo.style.height = '80vh';
     }
     // Project page
     else {
         const projectContentWrapper = document.getElementById('project-content-wrapper');
         projectTop.style.opacity = projectContentWrapper.style.opacity = '1';
+        if(window.innerWidth <= 980)
+        {
+            bigReelContainer.style.height = bigReelVideo.style.height = '28em';
+        }
     }
     bigReelFilter.style.opacity = bigReelFade.style.opacity = '1';
     nav.style.opacity = '1';
