7e4beb7d86200924294b9609a7116c480ac2b36a
ShadyLeeGamer <64636379+ShadyLeeGamer@users.noreply.github.com>
2023-07-19 00:07:38 +0100

    game jams other spinners, consistent pages..

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,12 +1,12 @@
 const projectTop = document.getElementById('project-top');
-isHomePage = projectTop == null;
+isProjectPage = projectTop != null;
 // $(document).ready(function() {
 //     $("#loader-wrapper").fadeOut("slow");
-//     setTimeout(BigReelIntro, isHomePage ? 2000 : 1000);
+//     setTimeout(BigReelIntro, isProjectPage ? 2000 : 1000);
 // });
 $(window).on("load", function() {
     $("#loader-wrapper").fadeOut("slow");
-    setTimeout(BigReelIntro, isHomePage ? 2000 : 1000);
+    setTimeout(BigReelIntro, isProjectPage ? 1000 : 2000);
 });
 
 // Big Reel
@@ -16,20 +16,19 @@ function BigReelIntro() {
     const bigReelFilter = document.querySelector('#big-reel #filter');
     const bigReelFade = document.querySelector('#big-reel #fade');
     const nav = document.getElementById('nav');
-    // Home page
-    if (isHomePage)
-    {
-				// height: 28em;
-        bigReelContainer.style.height = bigReelVideo.style.height = '80vh';
-    }
     // Project page
-    else {
+    if (isProjectPage)
+    {
         const projectContentWrapper = document.getElementById('project-content-wrapper');
         projectTop.style.opacity = projectContentWrapper.style.opacity = '1';
-        if(window.innerWidth <= 980)
+        if(window.innerWidth <= 980 && document.querySelector(".tabs") == null)
         {
             bigReelContainer.style.height = bigReelVideo.style.height = '28em';
         }
+    }
+    // Home page
+    else {
+        bigReelContainer.style.height = bigReelVideo.style.height = '80vh';
     }
     bigReelFilter.style.opacity = bigReelFade.style.opacity = '1';
     nav.style.opacity = '1';
