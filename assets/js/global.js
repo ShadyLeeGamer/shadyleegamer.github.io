74eaa099e8426c744bcec183201daddd590a8b3c
ShadyLeeGamer <64636379+ShadyLeeGamer@users.noreply.github.com>
2023-07-18 20:23:15 +0100

    loader test

---
+++ assets/js/global.js
@@ -0,0 +1,33 @@
+// Big Reel Intro
+
+// const projectTop = document.getElementById('project-top');
+// const isHomePage = projectTop == null;
+// setTimeout(ResizeTop, isHomePage ? 2000 : 1000);
+
+
+// Big Reel Intro
+// const projectTop = document.getElementById('project-top');
+// const isHomePage = projectTop == null;
+// setTimeout(ResizeTop, isHomePage ? 2000 : 1000);
+// function ResizeTop() {
+//     const bigReelContainer = document.getElementById('big-reel');
+//     const bigReelVideo = document.querySelector('#big-reel video');
+//     const bigReelFilter = document.querySelector('#big-reel #filter');
+//     const nav = document.getElementById('nav');
+//     // Home page
+//     if (projectTop == null)
+//     {
+//         const bigReelFade = document.querySelector('#big-reel #fade');
+//         bigReelContainer.style.height = bigReelVideo.style.height = '80vh';
+//         bigReelFade.style.opacity = '1';
+
+//     }
+//     // Project page
+//     else {
+//         const projectContentWrapper = document.getElementById('project-content-wrapper');
+//         projectTop.style.opacity = projectContentWrapper.style.opacity = '1';
+//     }
+
+//     bigReelFilter.style.opacity = '1';
+//     nav.style.opacity = '1';
+// }
---
+++ assets/js/loader.js
@@ -0,0 +1,27 @@
+const projectTop = document.getElementById('project-top');
+isHomePage = projectTop == null;
+$(window).on("load", function() {
+    $("#loader-wrapper").fadeOut("slow");
+    setTimeout(BigReelIntro, isHomePage ? 2000 : 1000);
+});
+
+// Big Reel
+function BigReelIntro() {
+    const bigReelContainer = document.getElementById('big-reel');
+    const bigReelVideo = document.querySelector('#big-reel video');
+    const bigReelFilter = document.querySelector('#big-reel #filter');
+    const bigReelFade = document.querySelector('#big-reel #fade');
+    const nav = document.getElementById('nav');
+    // Home page
+    if (isHomePage)
+    {
+        bigReelContainer.style.height = bigReelVideo.style.height = '80vh';
+    }
+    // Project page
+    else {
+        const projectContentWrapper = document.getElementById('project-content-wrapper');
+        projectTop.style.opacity = projectContentWrapper.style.opacity = '1';
+    }
+    bigReelFilter.style.opacity = bigReelFade.style.opacity = '1';
+    nav.style.opacity = '1';
+}
