5ab50de485e890018b13cab8575b7150964ad1cb
ShadyLeeGamer <64636379+ShadyLeeGamer@users.noreply.github.com>
2023-07-31 00:51:14 +0100

    game jam page with music player done, tab contents fixed

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -6,88 +6,47 @@ let currentTabContents = [];
 tabs.forEach(tab =>
     {
         tab.addEventListener('click', () => {
-            const targets = document.querySelectorAll(tab.dataset.tabTarget);
-            console.log(targets.length);
-            // let count = targets.length > 0 ? targets.length : 1;
-            for (let i = 0; i < targets.length; i++)
-            {
-                let target =  targets[i];
-
-                if(currentTabContents[i] != null)
-                {
-                    currentTabContents[i].classList.remove('active');
-                    
-                    if (currentTab != null)
-                        currentTab.firstElementChild.classList.add('spinner');
-                }
-    
-                if(target != currentTabContents[i])
-                {
-                    currentTabContents[i] = target;
-                    currentTabContents[i].classList.add('active');
-    
-                    currentTab = tab;
-                    currentTab.firstElementChild.classList.remove('spinner');
-                }
-                else
-                {
-                    currentTabContents[i] = null;
-                    currentTab = null;
-                }
-            }
+            OnTabClick(tab);
         });
 
         tab.addEventListener("mouseenter", () => {
             tab.firstElementChild.classList.remove('spinner');
-            // tab.firstElementChild.style.animation = "";
         });
-
         tab.addEventListener("mouseleave", () => {
             if (tab != currentTab)
             {
                 tab.firstElementChild.classList.add('spinner');
             }
         });
-    })
+    });
 
+function OnTabClick(tab)
+{
+    for (let i = 0; i < currentTabContents.length; i++)
+    {
+        currentTabContents[i].classList.remove('active');
+    }
 
-    // let currentTab;
-    // let currentTabContent;
-    // tabs.forEach(tab =>
-    //     {
-    //         tab.addEventListener('click', () => {
-    //             const target = document.querySelector(tab.dataset.tabTarget);
+    const tabContents = document.querySelectorAll(tab.dataset.tabTarget);
+    if(currentTab != tab)
+    {
+        if (currentTab != null)
+        {
+            currentTab.firstElementChild.classList.add('spinner');
+        }
+        currentTab = tab;
+        currentTab.firstElementChild.classList.remove('spinner');
+
+        currentTabContents = tabContents;
+    }
+    else
+    {
+        currentTabContents = [];
+        currentTab = null;
+    }
     
-    //             if(currentTabContent != null)
-    //             {
-    //                 currentTabContent.classList.remove('active');
-    //                 currentTab.firstElementChild.classList.add('spinner');
-    //             }
-    
-    //             if(target != currentTabContent)
-    //             {
-    //                 currentTabContent = target;
-    //                 currentTabContent.classList.add('active');
-    
-    //                 currentTab = tab;
-    //                 currentTabContent.firstElementChild.classList.remove('spinner');
-    //             }
-    //             else
-    //             {
-    //                 currentTabContent = null;
-    //                 currentTab = null;
-    //             }
-    //         });
-    
-    //         tab.addEventListener("mouseenter", () => {
-    //             tab.firstElementChild.classList.remove('spinner');
-    //         });
-    
-    //         tab.addEventListener("mouseleave", () => {
-    //             if (tab != currentTab)
-    //             {
-    //                 tab.firstElementChild.classList.add('spinner');
-    //             }
-    //         });
-    //     })
-    
+    for (let i = 0; i < currentTabContents.length; i++)
+    {
+        currentTabContents[i].classList.add('active');
+    }
+}
